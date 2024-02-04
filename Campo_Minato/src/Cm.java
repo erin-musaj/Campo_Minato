c9dad53eb95e9ca09504cbc4977ca2edeada3468
erin-musaj <erin.musaj@buonarroti.tn.it>
2024-02-04 20:56:28 +0100

    TODO: mine adiacenti

--- Campo_Minato/src/Cm.java
+++ Campo_Minato/src/Cm.java
@@ -1,9 +1,11 @@
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
 public class Cm {
 
     String[][] campo;
+    String[][] gioco;
 
     public Cm(int righe, int colonne){
         campo = new String[righe][colonne];
@@ -18,19 +20,36 @@ public class Cm {
     }
 
     public void posizionaMine(int percentuale){
-        LinkedList<Integer> celleDisp = new LinkedList<Integer>();
+        LinkedList<Integer> celleDisp = new LinkedList <>();
         for (int i = 0; i < campo.length*campo[0].length; i++) {
             celleDisp.add(i);
         }
 
+        int ncelle = (this.campo[0].length * this.campo.length);
         Random r = new Random();
 
-        for (int i = 0; i < percentuale; i++) {
+        for (int i = 0; i < ncelle; i++) {
             int celle = r.nextInt(celleDisp.size());
+            celleDisp.remove(celle);
             int riga = celle/this.campo[0].length;
             int colonna = celle % this.campo[0].length;
 
             this.campo[riga][colonna] = "-1";
+        }
+    }
+
+    public boolean eseguiClick(int riga, int colonna){
+        if(this.campo[riga][colonna].equals("-1")){
+            return false;
+        } else {
+            Queue<Integer> coda = new LinkedList<>();
+            coda.add(riga*this.campo[0].length + colonna);
+            while(coda.isEmpty()){
+                int a = 0;
+                if(riga!=0 && (campo[coda.peek()/this.campo[0].length][colonna]==0)){
+                    coda.add(riga-1*this.campo[0].length + colonna);
+                }
+            }
         }
     }
 
--- Campo_Minato/src/Main.java
+++ Campo_Minato/src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
+    public class CampoMinato {
+
+        private static final int RIGHE = 5; // Numero di righe del campo minato
+        private static final int COLONNE = 5; // Numero di colonne del campo minato
+
+        // Funzione per trovare le celle adiacenti
+        private static void trovaCelleAdiacenti(int[][] campoMinato, int riga, int colonna) {
+            for (int i = riga - 1; i <= riga + 1; i++) {
+                for (int j = colonna - 1; j <= colonna + 1; j++) {
+                    // Verifica se la cella è all'interno del campo minato
+                    if (i >= 0 && i < RIGHE && j >= 0 && j < COLONNE) {
+                        // Ignora la cella stessa
+                        if (i != riga || j != colonna) {
+                            System.out.println("Cella adiacente: (" + i + ", " + j + ")");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] campoMinato = {
+                {0, 1, 0, 0, 1},
+                {1, 0, 1, 0, 0},
+                {0, 1, 0, 1, 0},
+                {0, 0, 1, 0, 1},
+                {1, 0, 0, 1, 0}
+        };
+
+        int riga = 2;
+        int colonna = 2;
+
+        System.out.println("Celle adiacenti alla cella (" + riga + ", " + colonna + "):");
+        CampoMinato.trovaCelleAdiacenti(campoMinato, riga, colonna);
     }
 }
